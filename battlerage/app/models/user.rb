ec405ab6db8d42ec1d2348719d58d580050f8666
Valentin Genev <valentin_genev@abv.bg>
2014-09-14 18:01:11 +0600

    -Bootstrap... somewhat

--- battlerage/app/models/user.rb
+++ battlerage/app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  #attr_accessible :user_name, :email, :password, :password_confirmation
 end
+
+#from new.html.erb ... I got errors on these lines
+#<div><%= f.label :user_name %><br />
+#<%= f.text_field :user_name %></div> -->
